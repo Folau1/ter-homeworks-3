fd4589bd5fd49fcb18b27187dccf8df10aa154ad
Folau Aksenov <folau1@mail.ru>
2026-08-17 13:18:19 +0500

    Настроил S3 backend для remote state

--- 04/demonstration1/vms/providers.tf
+++ 04/demonstration1/vms/providers.tf
@@ -1,10 +1,28 @@
 terraform {
+  required_version = "~> 1.12.0"
+
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = "~>1.12.0"
+
+  backend "s3" {
+    bucket = "folau1-tfstate-bga82v"
+    key    = "terraform.tfstate"
+    region = "ru-central1"
+
+    use_lockfile = true
+
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
 }
 
 provider "yandex" {
